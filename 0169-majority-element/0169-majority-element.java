3c281934ab579d85d54a00ad81ae316607cea3d1
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2025-08-06 11:37:53 +0530

    Time: 1 ms (99.87%), Space: 53.5 MB (20.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,11 +1,4 @@
 class Solution {
-
-    static {
-        for (int i = 0; i < 500; i++) {
-            majorityElement(new int[] {0});
-        }
-    }
-
     public static int majorityElement(int[] nums) {
 
         int count=0;
