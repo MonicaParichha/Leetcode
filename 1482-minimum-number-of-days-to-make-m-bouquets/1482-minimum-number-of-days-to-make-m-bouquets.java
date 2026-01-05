0c4b48fd59ba335fe587b21c071ed2e561348a7d
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-01-06 00:45:50 +0530

    Time: 17 ms (72.79%), Space: 82.8 MB (11.09%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        // m >> bouquets
+        // k >> adjecent flowers
+        // n flowers
+        int n=bloomDay.length;
+        if(m*k>n){
+            return -1;
+        }
+        int l=1;
+        int r=maxEle(bloomDay);
+        int ans=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            boolean isFeasible= isFeasibleDay(bloomDay,mid,k,m);
+            if(isFeasible){
+                ans=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+    public boolean isFeasibleDay(int [] arr, int n, int k, int m){
+        
+        int count=0;
+        int len=arr.length;
+        int bouquets=0;
+        for(int i=0;i<len;i++){
+            if(n>=arr[i]){
+                count++;
+                if(count==k){
+                    bouquets++;
+                    count=0;
+                }
+            }else{
+                count=0;
+            }
+        }
+        return bouquets>=m;
+    }
+    public int maxEle(int [] arr){
+        int ans=Integer.MIN_VALUE;
+        for(int n: arr){
+            ans=Math.max(ans,n);
+        }
+        return ans;
+    }
+}
