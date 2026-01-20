44d9533fa2140bcda75468c5886e481af55fd3bb
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-01-20 23:14:18 +0530

    Time: 0 ms (100%), Space: 44.7 MB (38.67%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,35 +1,26 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n=nums.length;
-        for(int i=n-1;i>0;i--){
-            if(nums[i]>nums[i-1]){
-                int minIndex=findMin(nums,i,n-1);
-                if(minIndex!=-1){
-                    swap(nums,i-1,minIndex);
-                }
-                rev(nums,i,n-1);
-                return;
+        boolean flag=false;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                flag=true;
+                int index=findMinIndex(nums,nums[i],i);
+                swap(nums,i,index);
+                rev(nums,i+1,n-1);
+                break;
             }
         }
-        rev(nums,0,n-1);
-    }
-    public int findMin(int[] arr, int i, int j){
-        int ans=Integer.MAX_VALUE;
-        int index=-1;
-        for(int k=i;k<=j;k++){
-            if(arr[k]>arr[i-1] && arr[k]<=ans){
-                ans=arr[k];
-                index=k;
-            }
+        if(!flag){
+            rev(nums,0,n-1);
         }
-        return index;
     }
-    public void swap(int[] arr, int i, int j){
+    public void swap(int [] arr,int i,int j){
         int temp=arr[i];
         arr[i]=arr[j];
         arr[j]=temp;
     }
-    public void rev(int[] arr, int i, int j){
+    public void rev(int [] arr,int i, int j){
         while(i<=j){
             int temp=arr[i];
             arr[i]=arr[j];
@@ -37,5 +28,14 @@ class Solution {
             i++;
             j--;
         }
+    }
+    public int findMinIndex(int []arr,int num,int end){
+        int n=arr.length;
+        for(int i=n-1;i>=end;i--){
+            if(arr[i]>num){
+                return i;
+            }
+        }
+        return -1;
     }
 }
