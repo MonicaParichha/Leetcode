f61620b7dcd140b60acb05f5cdcb17bb2230058c
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-01-26 21:52:02 +0530

    Time: 33 ms (70.22%), Space: 60.6 MB (5.58%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        int n=nums.length;
         Arrays.sort(nums);
+        List<List<Integer>> arr=new ArrayList<>();
+        int n=nums.length;
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            int j=i+1;
-            int k=n-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum<0){
-                    j++;
-                }else if(sum>0){
-                    k--;
-                }else{
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int start=i+1;
+            int end=n-1;
+            while(start<end){
+                int sum=nums[i]+nums[start]+nums[end];
+                if(sum==0){
                     List<Integer> temp=new ArrayList<>();
                     temp.add(nums[i]);
-                    temp.add(nums[j]);
-                    temp.add(nums[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    while(j<k && nums[j]==nums[j-1]){
-                        j++;
-                    }
-                    while(j<k && nums[k]==nums[k+1]){
-                        k--;
-                    }
+                    temp.add(nums[start]);
+                    temp.add(nums[end]);
+
+                    arr.add(temp);
+
+                    start++;
+                    end--;
+
+                    while(start < end && nums[start]==nums[start-1]) start++;
+                    while(start < end && nums[end]==nums[end+1]) end--;
                 }
+                else if(sum<0) start++;
+                else end--;
             }
         }
-        return ans;
-        
+        return arr;
     }
 }
