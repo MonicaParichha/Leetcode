d17abb65af7111fd1ff670fa0938a422adc63e02
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-02-11 02:00:57 +0530

    Time: 11 ms (37.27%), Space: 59.4 MB (71.96%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode middle=middleNode(head);
+        ListNode leftHead=head;
+        ListNode rightHead=middle.next;
+        middle.next=null;
+        leftHead=sortList(leftHead);
+        rightHead=sortList(rightHead);
+        return merge(leftHead,rightHead);   
+    }
+
+    public ListNode middleNode(ListNode head){
+        if(head==null || head.next==null) return head;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+
+    public ListNode merge(ListNode head1, ListNode head2){
+
+        if(head1==null) return head2;
+        if(head2==null) return head1;
+
+        ListNode temp1=head1;
+        ListNode temp2=head2;
+        ListNode dummy=new ListNode(-1);
+        ListNode head=dummy;
+
+        while(temp1!=null && temp2!=null){
+            if(temp1.val<=temp2.val){
+                dummy.next=temp1;
+                temp1=temp1.next;
+            }
+            else{
+                dummy.next=temp2;
+                temp2=temp2.next;
+            }
+            dummy=dummy.next;
+        }
+        while(temp1!=null){
+            dummy.next=temp1;
+            temp1=temp1.next;
+            dummy=dummy.next;
+        }
+        while(temp2!=null){
+            dummy.next=temp2;
+            temp2=temp2.next;
+            dummy=dummy.next;
+        }
+        return head.next;
+    }
+}
