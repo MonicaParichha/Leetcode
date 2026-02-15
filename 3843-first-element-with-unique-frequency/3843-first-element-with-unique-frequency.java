58d84117f1a4d4cdf44efb3edbe53526ef152ca2
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-02-15 17:07:33 +0530

    Time: 0 ms (100%), Space: 226.3 MB (17.21%) - LeetHub

---
+++ 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int firstUniqueFreq(int[] nums) {
+        Map<Integer,Integer> map=new LinkedHashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        Map<Integer,Integer> map2=new LinkedHashMap<>();
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            map2.put(entry.getValue(),map2.getOrDefault(entry.getValue(),0)+1);
+        }
+        
+        int ans=getUniqueKey(map2);
+        
+        if(ans==-1) return -1;
+
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            if(entry.getValue().equals(ans)){
+                return entry.getKey();
+            }
+        }
+        return -1;
+        
+    }
+    
+    public int getUniqueKey(Map<Integer,Integer> map){
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            if(entry.getValue()==1) return entry.getKey();
+        }
+        return -1;
+    }
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (FileWriter writer = new FileWriter("display_runtime.txt"))
+            {
+                writer.write("0");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            }
+        ));
+    }
+}
