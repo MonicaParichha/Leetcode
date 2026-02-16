4907d59aa919342a4c0f6e4fdbc50305899a07e9
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-02-16 22:43:00 +0530

    Time: 2 ms (14.3%), Space: 44 MB (42.13%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,28 +1,27 @@
 class Solution {
     public int myAtoi(String s) {
-        long ans=0;
         s=s.trim();
-        int n=s.length();
-        int isNegative=1;
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='-' && i==0){
-                isNegative=-1;
-            }
-            else if(i==0 && s.charAt(i)=='+') isNegative=1;
-            else if(s.charAt(i)-'0'>=0 && s.charAt(i)-'0'<=9){
-                ans=ans*10;
-                ans+=(s.charAt(i)-'0');
-
-                if(ans*isNegative>Integer.MAX_VALUE) return Integer.MAX_VALUE;
-                if(ans*isNegative<Integer.MIN_VALUE) return Integer.MIN_VALUE;    
-                
-            }else{
-                break;
-            }
+        if(s.length()==0) return 0;
+        int index=0;
+        int sign=1;
+        if(s.charAt(0)=='+' || s.charAt(0)=='-'){
+            index++;
+            if(s.charAt(0)=='-') sign=-1;
         }
+        long ans=0;
+        ans=helper(sign,s,index, ans);
+        return (int)ans;
+    }
+    public long helper(int sign, String s,int index, long ans){
+        
+        if(index>=s.length() || !Character.isDigit(s.charAt(index))) return ans*sign;
 
+        long newAns= (long)((ans*10)+(s.charAt(index)-'0'));
+
+        if(newAns*sign>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(newAns*sign<Integer.MIN_VALUE) return Integer.MIN_VALUE;
         
 
-        return (int)ans*isNegative;
-    }
+        return helper(sign,s,index+1,newAns);
+    } 
 }
