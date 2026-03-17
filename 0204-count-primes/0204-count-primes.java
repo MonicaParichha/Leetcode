e8328197dc6333670101d40f21283fc2c5d47632
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-03-17 14:42:10 +0530

    Time: 427 ms (5.08%), Space: 162.7 MB (6.5%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count=0;
+        List<Integer> primes=seive(n);
+        for(int i=2;i<primes.size();i++){
+            if(primes.get(i)==1){
+                count++;
+            }
+        }
+        return count;
+    }
+    public List<Integer> seive(int n){
+        List<Integer> primes=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            primes.add(1);
+        }
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(primes.get(i)==1){
+                for(int j=i*i;j<n;j+=i){
+                    primes.set(j,0);
+                }
+            }
+        }
+        return primes;
+    }
+}
