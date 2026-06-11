7f64cb8efb8ac229b002a48167224a1827b2cbca
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-06-11 18:28:25 +0530

    Time: 775 ms (5.1%), Space: 47.9 MB (69.98%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n=hand.length;
+        HashMap<Integer, Integer> map=new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            map.put(hand[i], map.getOrDefault(hand[i],0)+1);
+        }
+
+        int m=map.size();
+
+        while(!map.isEmpty()){
+            int min=Collections.min(map.keySet());
+            map.put(min,map.get(min)-1);
+            if(map.get(min)==0){
+                map.remove(min);
+            }
+            for(int i=1;i<groupSize;i++){
+                if(map.containsKey(min+i)){
+                    map.put(min+i,map.get(min+i)-1);
+                    if(map.get(min+i)==0){
+                        map.remove(min+i);
+                    }
+                }else{
+                    return false;
+                }
+            }
+        }
+        
+        return map.isEmpty();
+    }
+}
