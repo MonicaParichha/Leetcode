95dff1ea59e423b6d3d2daff64ed4e1ae3e5ef81
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-08-06 22:56:38 +0530

    Time: 0 ms (100%), Space: 44.7 MB (33.65%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,38 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n=nums.length;
-        boolean isThere=false;
+        boolean flag=false;
         for(int i=n-1;i>0;i--){
             if(nums[i]>nums[i-1]){
-                int min=findMin(nums,i-1,n-1);
-                if(min!=-1) swap(nums,min,i-1);
-                reverse(nums,i,n-1);
-                isThere=true;
-                break;
+                flag=true;
+                int index=smallestIndex(nums,n-1,i,i-1);
+                if(index==-1) return; 
+                swap(nums,i-1,index);
+                rev(i,n-1,nums);
+                return;
             }
         }
-        if(!isThere){
-            reverse(nums,0,n-1);
-        }
+        if(flag==false) rev(0,n-1,nums);
     }
-    public int findMin(int[] arr, int i, int j){
-        int ans=-1;
-        for(int s=j;s>i;s--){
-            if(arr[i]<arr[s]){
-                ans=s;
-                break;
-            }
-        }
-        return ans;
-    }
-    public void swap(int[] arr, int i, int j){
+    public static void swap(int[] arr, int i, int j){
         int temp=arr[i];
         arr[i]=arr[j];
         arr[j]=temp;
     }
-    public void reverse(int[] arr, int i, int j){
-        while(i<j){
-            swap(arr,i,j);
+    public int smallestIndex(int[] arr, int i, int j, int ind){
+        for(int k=i;k>0;k--){
+            if(arr[ind]<arr[k]){
+                return k;
+            }
+        }
+        return -1;
+    }
+    public void rev(int i, int j, int[] nums){
+        while(i<=j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
             i++;
             j--;
         }
