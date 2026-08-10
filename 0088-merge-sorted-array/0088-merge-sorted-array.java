4df339eb9266d334ba9109ed041a83b852ff609c
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-08-11 00:09:14 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.64%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,43 +1,31 @@
 class Solution {
-    public void merge(int[] arr1, int n, int[] arr2, int m) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
         int len=m+n;
-
-        // Gap method - Shell Sort
-        int gap=(len/2) + (len%2);
-        
-        while(gap>0){
-            int left=0;
-            int right=left+gap;
-            while(right<len){
-                //arr1 and arr2
-                if(left<n && right>=n){
-                    swap(arr1, arr2, left, right-n);
-                }
-                //arr2 and arr2
-                else if(left>=n){
-                    swap(arr2, arr2, left-n, right-n);
-                }
-                //arr1 and arr1
-                else{
-                    swap(arr1, arr1, left, right);
-                }
-                left++;
-                right++;
+        int[] ans=new int[len];
+        int i=0,j=0,k=0;
+        while(i<m && j<n){
+            if(nums1[i]<=nums2[j]){
+                ans[k]=nums1[i];
+                i++;
             }
-            if(gap==1) break;
-            gap=(gap/2)+(gap%2);
+            else{
+                ans[k]=nums2[j];
+                j++;
+            }
+            k++;
         }
-        for(int i=0;i<m;i++){
-            arr1[n+i]=arr2[i];
+        while(i<m){
+            ans[k]=nums1[i];
+            i++;
+            k++;
         }
-    }
-
-
-    public void swap(int [] arr1, int[] arr2, int i, int j){
-        if(arr1[i]>arr2[j]){
-            int temp=arr1[i];
-            arr1[i]=arr2[j];
-            arr2[j]=temp;
+        while(j<n){
+            ans[k]=nums2[j];
+            j++;
+            k++;
+        }
+        for(int a=0;a<len;a++){
+            nums1[a]=ans[a];
         }
     }
 }
