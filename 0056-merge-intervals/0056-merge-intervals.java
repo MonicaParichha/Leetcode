b51952e475e59f0769398f09343b80e5c83bab70
MonicaParichha <98113320+MonicaParichha@users.noreply.github.com>
2026-08-10 23:58:38 +0530

    Time: 11 ms (14.93%), Space: 49 MB (61.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,26 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b)->Integer.compare(a[0], b[0]));
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
         int n=intervals.length;
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
+
         for(int i=0;i<n;i++){
-            ArrayList<Integer> temp=new ArrayList<>();
-            if(arr.isEmpty() || intervals[i][0] > arr.get(arr.size()-1).get(1)){
+            if(ans.isEmpty() || intervals[i][0]>ans.get(ans.size()-1).get(1)){
+                ArrayList<Integer> temp=new ArrayList<>();
                 temp.add(intervals[i][0]);
                 temp.add(intervals[i][1]);
-                arr.add(temp);
+                ans.add(temp);
             }else{
-                int lastIndex=arr.size()-1;
-                int maxEle=Math.max(arr.get(lastIndex).get(1), intervals[i][1]);
-                arr.get(lastIndex).set(1, maxEle);
+                int maxEnd=Math.max(intervals[i][1], ans.get(ans.size()-1).get(1));
+                ans.get(ans.size()-1).set(1,maxEnd);
             }
-            
         }
-        int[][] ans=new int[arr.size()][2];
-        for(int i=0;i<arr.size();i++){
-            ans[i][0] = arr.get(i).get(0);
-            ans[i][1] = arr.get(i).get(1);
+        int [][] ans1=new int[ans.size()][2];
+        for(int i=0;i<ans.size();i++){
+            for(int j=0;j<2;j++){
+                ans1[i][j]=ans.get(i).get(j);
+            }
         }
-        return ans;
+        return ans1;
     }
 }
